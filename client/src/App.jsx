5256d66ac9568fb48ec9528f4029df4cdb287edf
abdul8840 <abdul14941naseer@gmail.com>
2025-01-02 18:23:08 +0530

    Project setup with tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='text-4xl font-bold text-slate-500'>
+      Hellow world
+    </div>
+  )
+}
+
+export default App
